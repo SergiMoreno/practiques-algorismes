b7210d27238e62359d18da334eacb82c913bd794
SergiMoreno <morenoperez.sergi@gmail.com>
2023-03-24 00:20:22 +0100

    Solution with list of stacks

--- Practica2/src/practica2/controller/Controller.java
+++ Practica2/src/practica2/controller/Controller.java
@@ -1,5 +1,7 @@
 package practica2.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import practica2.Event;
 import practica2.EventListener;
@@ -19,20 +21,11 @@ class PieceState {
     final public int posx;
     final public int posy;
     final public int movement;
-    final public int pieceIndex;
     
-    public PieceState (int x, int y, int movement, int pieceIndex) {
+    public PieceState (int x, int y, int movement) {
         this.posx = x;
         this.posy = y;
         this.movement = movement;
-        this.pieceIndex = pieceIndex;
-    }
-    
-    public boolean isEqual(PieceState other) {
-        return this.posx == other.posx 
-                && this.posy == other.posy 
-                && this.movement == other.movement 
-                && this.pieceIndex == other.pieceIndex;
     }
 }
 
@@ -54,20 +47,16 @@ public class Controller extends Thread implements EventListener {
         int turn;
         int boardSize = model.getBoardSize();
         
-        //PieceState [] initStates = new PieceState[model.getNumPieces()];
-        Stack<PieceState> stack = new Stack<PieceState>();   // Stack of states
-        /*for (int i = 0; i < model.getNumPieces(); i++) { // Initialize each stack
-            int x = model.getPiecePosX(i);
-            int y = model.getPiecePosY(i);
-            PieceState initialState = new PieceState(x,y,0,i);
-            initStates[i] = initialState;
-            stack.add(initialState);
-        }*/
+        List<Stack<PieceState>> states = new ArrayList<Stack<PieceState>>();   // List of stacks of states
+        for (int i = 0; i < model.getNumPieces(); i++) { // Initialize each stack
+            Stack<PieceState> stack = new Stack<PieceState>();
+            states.add(stack);
+        }
         
         // Start algorithm
         while (!isSolution && !noSolution) {
             turn = model.getPieceTurn(); // Piece turn
-            //Stack<PieceState> currentStack = states.get(turn); // Piece states
+            Stack<PieceState> currentStack = states.get(turn); // Piece states
             
             int posx = model.getPiecePosX(turn);
             int posy = model.getPiecePosY(turn);
@@ -79,35 +68,41 @@ public class Controller extends Thread implements EventListener {
                 
                 int x = movex + posx;
                 int y = movey + posy;
-                PieceState state = new PieceState(x, y, movement, turn);
+                PieceState state = new PieceState(x, y, movement);
                 
                 boolean valid = model.isValidMovement(x, y);
                 if (valid) {
-                    stack.add(state);
+                    currentStack.add(state);
                 }
             }
             
-            noSolution = stack.empty();
+            noSolution = currentStack.empty();
             if (!noSolution) {
-                PieceState current = stack.pop();
+                PieceState current = currentStack.pop();
 
                 // Get the last valid state on the stack
-                //boolean hasToPrune = false;
-                boolean valid = false;
-                if (current.pieceIndex == turn) valid = model.isFreeCell(current.posx, current.posy, current.movement, current.pieceIndex);
-                //while ((!valid || current.pieceIndex != turn) && !stack.empty()) {
-                //while ((!valid && !stack.empty()) || current.pieceIndex != turn) {
-                while (!valid && !stack.empty()) {
-                    current = stack.pop();
-                    if (current.pieceIndex == turn) valid = model.isFreeCell(current.posx, current.posy, current.movement, current.pieceIndex);
+                boolean hasToPrune = false;
+                boolean valid = model.isFreeCell(current.posx, current.posy, current.movement, turn);
+                while (!valid && !currentStack.empty()) {
+                    current = currentStack.pop();
+                    valid = model.isFreeCell(current.posx, current.posy, current.movement, turn);
+                    hasToPrune = true;
                 }
                 
-                if (current.movement == 1) {
-                    System.out.println("");
+                // Prune non-necessary states of each stack in the list
+                if (hasToPrune) {
+                    for (int i = 0; i < states.size(); i++) {
+                        Stack<PieceState> others = states.get(i);
+                        if (!others.isEmpty()) {
+                            PieceState top = others.peek();
+                            while (top.movement > current.movement) {
+                                others.pop();
+                                if (others.isEmpty()) break;
+                                top = others.peek();
+                            }
+                        }
+                    }
                 }
-                /*while (!stack.isEmpty() && stack.peek().movement > current.movement) {
-                    stack.pop();
-                }*/
                 
                 if (current.movement < model.getMovement()) // Prune board cells
                     model.prune(current.movement, turn);
@@ -120,13 +115,11 @@ public class Controller extends Thread implements EventListener {
                 // Check if all the cells have been occupied
                 if (model.getOccupiedCells() == (boardSize * boardSize)) 
                     isSolution = true;
-                /*if (current.isEqual(initStates[turn])) {
-                    noSolution = true;
-                }*/
             }
             
             try {
                 Thread.sleep(10000/(speed*20));
+                //Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
             }
