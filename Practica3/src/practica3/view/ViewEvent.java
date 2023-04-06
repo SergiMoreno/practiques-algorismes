aaf94149642ae90443b2abb0928907f7125eccfb
SergiMoreno <morenoperez.sergi@gmail.com>
2023-04-06 19:41:14 +0200

    Corrections after class, problems with the combination process for log algorithm

--- Practica3/src/practica3/view/ViewEvent.java
+++ Practica3/src/practica3/view/ViewEvent.java
@@ -11,15 +11,17 @@ import practica3.Event;
  */
 public class ViewEvent extends Event {
     public ViewEventType type;
-    public ArrayList<Integer> index;
+    public ArrayList<Integer> indexs;
     
-    public ViewEvent(ArrayList<Integer> index) {
+    // Event when the controller ends its execution and it can show which points
+    // have been found
+    public ViewEvent(ArrayList<Integer> indexs) {
         super(EventType.View);
-        this.type = ViewEventType.REFRESH;
-        this.index = index;
+        this.type = ViewEventType.SHOW_RESULT;
+        this.indexs = indexs;
     }
     
     enum ViewEventType {
-        REFRESH
+        SHOW_RESULT
     }
 }
