066c1d0bb56400c84b234b8918436cd60eadd9dd
SergiMoreno <morenoperez.sergi@gmail.com>
2023-05-16 13:57:31 +0200

    Algorithm implementation

--- Practica5/src/practica5/view/ViewEvent.java
+++ Practica5/src/practica5/view/ViewEvent.java
@@ -7,9 +7,13 @@ import practica5.Event;
  * @author usuario
  */
 public class ViewEvent extends Event {
+    public ViewEventType type;
+    public double [] results;
 
     public ViewEvent() {
         super(EventType.View);
+        
+        this.type = ViewEventType.SHOW_RESULT;
     }
     
     enum ViewEventType {
